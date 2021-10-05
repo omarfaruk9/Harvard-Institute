127c7b3b8e5a05c59fa7c5139c6b8a48f26e2a53
omarfaruk9 <fh5230365@gamil.com>
2021-10-05 13:28:30 +0600

    back bnt  add

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import notfound from "../../images/not-found.jpg";
 
 const NotFound = () => {
@@ -6,6 +7,12 @@ const NotFound = () => {
         <div>
             <h1 className="text-center text-warning my-5 py-5">Page not Found 404</h1>
             <img className="w-50 mx-auto d-block" src={notfound} alt="" />
+
+            <div className="text-center">
+                <Link to="/home">
+                    <button className="btn-lg btn-danger mt-3">Back to home</button>
+                </Link>
+            </div>
         </div>
     );
 };
