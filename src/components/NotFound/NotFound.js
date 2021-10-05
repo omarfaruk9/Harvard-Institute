d4d3127466aec7dc5814d15fad212368259d32bc
omarfaruk9 <fh5230365@gamil.com>
2021-10-05 13:20:40 +0600

    submit done

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import notfound from "../../images/not-found.jpg";
 
 const NotFound = () => {
     return (
         <div>
             <h1 className="text-center text-warning my-5 py-5">Page not Found 404</h1>
+            <img className="w-50 mx-auto d-block" src={notfound} alt="" />
         </div>
     );
 };
